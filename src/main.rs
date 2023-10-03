d3bd1a90a66a3f0151c5b938385647cc41d3cd1f
Cloudiful <45996018+cloudiful@users.noreply.github.com>
2023-10-04 02:28:39 +0800

    add run command function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use chrono;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let current_date = chrono::offset::Local::now().date_naive();
+
+    run("dir")
+}
+
+/// this function is to run command and print output
+fn run(command: &str) {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/C")
+            .arg(command)
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("failed to execute process")
+    };
+
+    let output_str = String::from_utf8(output.stdout).expect("read output error");
+
+    println!("{}", output_str);
 }
